e50884211bab26d787245b7df47698de2d45db81
KerwinTsaiii <kerwtsai@amd.com>
2026-03-03 11:37:31 +0800

    Update theme

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
 # MyST parser settings
